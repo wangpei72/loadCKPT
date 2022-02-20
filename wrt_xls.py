f98891ee66282cbea5cfedb8c91eed7c937e6c7f
wangpei72 <m1737506964@163.com>
2022-02-20 15:54:50 +0800

    modify xls format

--- wrt_xls.py
+++ wrt_xls.py
@@ -7,8 +7,11 @@ if __name__ == "__main__":
     acc_array = np.load('./test_accu20220220145027.npy', allow_pickle=True)
     workbook = wt.Workbook()
     sheet = workbook.add_sheet('sheet1')
+    sheet.write(0, 0, 'test id')
+    sheet.write(1, 0, 'test accuracy')
     for i in range(20):
-        sheet.write(1, i+1, acc_array[i])
+        sheet.write(0, i + 1, i + 1)
+        sheet.write(1, i + 1, acc_array[i])
 
     time_str = time.strftime("%Y%m%d%H%M%S", time.localtime())
     workbook.save('test' + time_str + '.xls')
