e63f58087bbd7b9c9177f1c86e68595a0a101324
pei Wang <44791528+wangpei72@users.noreply.github.com>
2022-03-01 00:34:39 +0800

    add res for 01

--- wrt_xls.py
+++ wrt_xls.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     # di_array = np.load('res_avg/di_avg.npy')
     # spd_array = np.load('res_avg/spd_avg.npy')
     workbook = wt.Workbook()
-    while id_list_cnt < 5:
-        di_array = np.load('testres/di_res' + id_list[id_list_cnt] + '.npy')
-        spd_array = np.load('testres/spd_res' + id_list[id_list_cnt] + '.npy')
+    while id_list_cnt < 1:
+        di_array = np.load('bank-testres/di_res' + id_list[id_list_cnt] + '.npy')
+        spd_array = np.load('bank-testres/spd_res' + id_list[id_list_cnt] + '.npy')
         sheet = workbook.add_sheet('spd_and_di' + id_list[id_list_cnt])
         sheet.write(0, 0, 'test id')
         sheet.write(1, 0, 'disparate_impact')
@@ -22,5 +22,5 @@ if __name__ == "__main__":
             sheet.write(1, i + 1, str(di_array[i]))
             sheet.write(2, i + 1, str(spd_array[i]))
         id_list_cnt += 1
-    workbook.save('xls_file/raw_group_fairness_in_5_tests.xls')
+    workbook.save('./bank-testres/xls_file/raw_group_fairness_in_5_tests.xls')
     print('test res saved as xls file.')
